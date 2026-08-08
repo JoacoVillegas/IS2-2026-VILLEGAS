b9308b65487d30b12a5963c7c0d3d3b4fa983c29
Joaquin Villegas <cocoproman11@gmail.com>
2026-08-08 08:22:07 -0300

    formulario ejercicio 1

---
+++ ejercicio 1/formulario/script.js
@@ -0,0 +1,18 @@
+document.getElementById('miform').addEventListener('submit', function(event){
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+
+    if (username.length < 8){
+        console.log('El usuario debe tener mas de 8 caracteres');
+        return
+    }
+    if (username && email){
+        console.log(`Usuario: ${username}`)
+        console.log(`Email: ${email}`)
+        console.log('todo correcto')
+
+    } else {
+        console.log("Algunos campos estan vacios")
+    }
+})
