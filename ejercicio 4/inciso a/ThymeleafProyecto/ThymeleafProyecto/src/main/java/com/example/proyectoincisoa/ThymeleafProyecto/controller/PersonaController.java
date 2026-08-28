403f581ebd72d37f2adad9c13bc07ba22b55e3a6
Joaquin Villegas <cocoproman11@gmail.com>
2026-08-28 10:27:41 -0300

    avance inciso a

---
+++ ejercicio 4/inciso a/ThymeleafProyecto/ThymeleafProyecto/src/main/java/com/example/proyectoincisoa/ThymeleafProyecto/controller/PersonaController.java
@@ -0,0 +1,30 @@
+package com.example.proyectoincisoa.ThymeleafProyecto.controller;
+
+import com.example.proyectoincisoa.ThymeleafProyecto.model.Persona;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PersonaController {
+    @GetMapping("/")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("persona", new Persona());
+        return "formulario";
+    }
+
+    @PostMapping("/procesar")
+    public String procesarFormulario(Persona persona, Model model) {
+        model.addAttribute("persona", persona);
+
+        if (persona.getEdad()>= 18) {
+            model.addAttribute("tipoEdad", "Es mayor de edad");
+        } else {
+            model.addAttribute("tipoEdad", "Es menor de edad");
+        }
+
+        return "resultado";
+    }
+}
