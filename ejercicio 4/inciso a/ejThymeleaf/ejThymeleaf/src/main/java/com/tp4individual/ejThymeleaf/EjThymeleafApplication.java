23613b99521ebc4fcff6fb0c5fe22a7e98aaf227
Joaquin Villegas <cocoproman11@gmail.com>
2026-08-28 09:12:29 -0300

    inicio individual tp 4

---
+++ ejercicio 4/inciso a/ejThymeleaf/ejThymeleaf/src/main/java/com/tp4individual/ejThymeleaf/EjThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tp4individual.ejThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjThymeleafApplication.class, args);
+	}
+
+}
---
+++ ejercicio 4/inciso a/ejThymeleaf/ejThymeleaf/src/test/java/com/tp4individual/ejThymeleaf/EjThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp4individual.ejThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
